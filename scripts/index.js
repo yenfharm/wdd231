cfcd17dc21c5fc5fee20e0ff57738afc9e032e02
yenfharm <yenfhael.arm@gmail.com>
2024-06-14 14:06:04 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
   // Update the last modified date
-  const lastModified = document.lastModified;
-  document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
-  
+const lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
