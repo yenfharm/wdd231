f2b8f11c02b7b7c0df17235e48aab2cbfd8a2f83
yenfharm <yenfhael.arm@gmail.com>
2024-06-16 19:58:38 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,12 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
   // Update the last modified date
 const lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
